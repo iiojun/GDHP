6e42879cfb2d5784d9202e6f7b9fa7cfffe82302
Jun Iio <iiojun@tamacc.chuo-u.ac.jp>
2019-03-10 11:58:08 +0900

    rearranged for refactoring

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 const COLORS = [ 'crimson', 'forestgreen', 'yellow', 'royalblue',
                  'saddlebrown', 'hotpink', 'darkorange', 'darkmagenta' ];
-const NUM_OF_DISKS = COLORS.length;
+const N_DISKS = COLORS.length;
 const BASE_LENGTH = 200;
 const C_WIDTH  = 3.732 * BASE_LENGTH;
 const C_HEIGHT = 3.500 * BASE_LENGTH;
@@ -9,18 +9,18 @@ const POLE_R = 15;
 const POSITIONS = { 'Source'      : [0.268, 0.714],
                     'Auxiliary'   : [0.500, 0.286],
                     'Destination' : [0.732, 0.714] };
-const FLASHING_COUNTER = 20;
+const FLASH_CTR = 20;
 const STEPS = 30;
 
 class Vector {
-  constructor(x,y) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 }
 
 class Position extends Vector {
-  constructor(x,y) { super(x,y); }
+  constructor(x, y) { super(x, y); }
 
   move(vec) {
     this.x += vec.x;
@@ -32,24 +32,29 @@ class Disk {
   constructor(level) {
     this.level = level;
     this.color = COLORS[level];
-    this.r = (DISK_R-POLE_R)*(NUM_OF_DISKS-level)/NUM_OF_DISKS + POLE_R;
+    this.r = (DISK_R-POLE_R)*(N_DISKS-level)/N_DISKS + POLE_R;
+  }
+
+  draw(pos) {
+    stroke('black');
+    fill(this.color);
+    ellipse(pos.x, pos.y, 2*this.r);
   }
 }
 
 class MovingDisk extends Disk {
-  constructor(level,from,to) {
+  constructor(level, from, to) {
     super(level); 
-    [sx,sy] = [from.pos.x,from.pos.y];
-    [dx,dy] = [to.pos.x,  to.pos.y];
-    this.pos = new Position(sx,sy);
-    this.mvec = new Vector((dx-sx)/STEPS,(dy-sy)/STEPS);
+    this.pos = new Position(from.pos.x, from.pos.y);
+    this.vec = new Vector((to.pos.x-from.pos.x)/STEPS, 
+                          (to.pos.y-from.pos.y)/STEPS);
     this.move_ctr = 0;
     this.from = from;
     this.to = to;
   }
 
   step_forward() {
-    this.pos.move(this.mvec);
+    this.pos.move(this.vec);
     this.move_ctr++;
   }
 
@@ -66,12 +71,37 @@ class Tower {
   constructor(name, disks, direction=null) {
     this.name = name;
     this.disks = [];
-    for (var i = 0; i < disks; i++) {
-      this.disks.push(new Disk(i));
-    }
+    for (var i = 0; i < disks; i++) { this.disks.push(new Disk(i)); }
     this.direction = direction;
-    this.moving = false;
+    var rx, ry;
+    [rx,ry] = POSITIONS[name];
+    this.pos = new Position(rx*C_WIDTH, ry*C_HEIGHT);
+
+    this.exchanging = false;
     this.flash_ctr = 0;
+  }
+
+  draw() {
+    var pos  = this.pos;
+    var pos2 = this.direction.pos;
+    var sx, sy, dx, dy, r;
+
+    // draw disks
+    this.disks.forEach(function(d) { d.draw(pos) })
+
+    // draw a pole
+    stroke('brown');
+    fill(this.exchanging & (this.flash_ctr < FLASH_CTR/2) 
+      ? 'gold' : 'white');
+    ellipse(pos.x, pos.y, 2*POLE_R);
+
+    // draw a direction
+    stroke('navy');
+    [sx, sy] = [pos.x,  pos.y ];
+    [dx, dy] = [pos2.x, pos2.y];
+    r = POLE_R / Math.sqrt((dx-sx)*(dx-sx)+(dy-sy)*(dy-sy));
+    [dx, dy] = [(dx-sx)*r+sx, (dy-sy)*r+sy];
+    line(sx, sy, dx, dy);
   }
 
   get toplevel() {
@@ -81,99 +111,69 @@ class Tower {
   }
 }
 
-var src = new Tower('Source', NUM_OF_DISKS);
+var src = new Tower('Source', N_DISKS);
 var aux = new Tower('Auxiliary',   0, src);
 var dst = new Tower('Destination', 0, src);
-// In the case of NUM_OF_DISKS is odd, 
-// the src must face the src.
+// In the case of N_DISKS is odd, the src must face the src.
 // Otherwise, the src faces the aux.
 src.direction = (COLORS.length % 2 == 1) ? dst : aux;
 
-// the reference to moving disk is stored to this variable.
+// the reference to the moving disk is stored to this variable.
 var moving_disk = null;
 
-function setup() {
-  createCanvas(C_WIDTH, C_HEIGHT);
-  frameRate(30);
-  [src,aux,dst].forEach(function(t) {
-    [rx,ry] = POSITIONS[t.name];
-    t.pos = new Position(rx * C_WIDTH, ry * C_HEIGHT);
-  })
-}
-
-function base_drawing() {
+function draw_stacking_disks() {
   background('beige');
-
-  [src,aux,dst].forEach(function(t) {
-    // draw disks
-    t.disks.forEach(function(d) {
-      stroke('black');
-      fill(d.color);
-      ellipse(t.pos.x,t.pos.y,2*d.r);
-    })
-
-    // draw a pole
-    stroke('brown');
-    fill(t.moving & (t.flash_ctr < FLASHING_COUNTER/2) ? 'gold' : 'white');
-    ellipse(t.pos.x,t.pos.y,2*POLE_R);
-
-    // draw a direction
-    stroke('navy');
-    [sx, sy] = [t.pos.x, t.pos.y];
-    [dx, dy] = [t.direction.pos.x, t.direction.pos.y];
-    r = POLE_R / Math.sqrt((dx-sx)*(dx-sx)+(dy-sy)*(dy-sy));
-    [dx, dy] = [(dx-sx)*r+sx, (dy-sy)*r+sy];
-    line(sx,sy,dx,dy);
-  })
+  [src, aux, dst].forEach(function(t) { t.draw(); })
 }
 
 function flash_poles() {
-  [src,aux,dst].forEach(function(t) {
-    t.moving = (t.direction.direction === t);
+  [src, aux, dst].forEach(function(t) {
+    t.exchanging = (t.direction.direction === t);
     t.flash_ctr += 1;
-    t.flash_ctr %= FLASHING_COUNTER;
+    t.flash_ctr %= FLASH_CTR;
   })
 }
 
-function pop_disk(src,aux,dst) {
-  var towers = [src,aux,dst].filter(t => t.moving);
-  var idx,from,to;
+function pop_disk(src, aux, dst) {
+  var towers = [src, aux, dst].filter(t => t.exchanging);
+  var idx, from, to;
   idx = (towers[0].toplevel > towers[1].toplevel) ? 0 : 1;
   [from, to] = [towers[idx], towers[1-idx]];
-  return new MovingDisk(from.disks.pop().level,from,to);
+  return new MovingDisk(from.disks.pop().level, from, to);
 }
 
 function draw_moving_disk() {
   var d = moving_disk;
   d.step_forward();
-  stroke('black');
-  fill(d.color);
-  ellipse(d.pos.x,d.pos.y,2*d.r);
+  d.draw(d.pos);
   return d.finish_p();
 }
 
 function turn() {
-  [moving_disk.from,moving_disk.to].forEach(function(t) {
-    t.direction = ([src,aux,dst]
+  [moving_disk.from, moving_disk.to].forEach(function(t) {
+    t.direction = ([src, aux, dst]
       .filter(x => (x !== t) && (x !== t.direction)))[0];
-    t.moving = false;
+    t.exchanging = false;
   })
 }
 
+function setup() {
+  createCanvas(C_WIDTH, C_HEIGHT);
+  frameRate(30);
+}
+
 function draw() {
-  // base drawing
-  base_drawing();
+  // draw three towers and disks stacking on the towers
+  draw_stacking_disks();
 
   // find two exchange-towers out of three
   flash_poles();
 
-  // start moving
+  // start exchanging
   if (moving_disk == null) {
-    moving_disk = pop_disk(src,aux,dst);
-  } else {
-    if (draw_moving_disk()) {
-      turn();
-      moving_disk = null;
-    }
-  }
+    moving_disk = pop_disk(src, aux, dst);
+  } else if (draw_moving_disk()) {
+    turn();
+    moving_disk = null;
+  } // else { do nothing }
 }
